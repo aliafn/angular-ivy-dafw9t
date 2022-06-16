ee7f06acc4b03218b70fdff193dd4e8bff52e2d0
aliafn <alimortadafneish36@gmail.com>
2022-06-16 15:39:01 +0300

    2 files modified

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -6,43 +6,48 @@ import { Passenger } from '../../models/passenger.interface';
   templateUrl: './passenger-dashboard.component.html',
   styleUrls: ['./passenger-dashboard.component.scss'],
 })
-export class PassengerDashboardComponent {
-  private passengers: Passenger[] = [
-    {
-      id: 1,
-      fullname: 'ali1',
-      checkedIn: true,
-      checkedInDate: 1490742000000,
-      children: [
-        { name: 'Jack', age: 10 },
-        { name: 'Nana', age: 5 },
-      ],
-    },
-    {
-      id: 2,
-      fullname: 'ali2',
-      checkedIn: false,
-      checkedInDate: null,
-      children: null,
-    },
-    {
-      id: 3,
-      fullname: 'ali3',
-      checkedIn: true,
-      checkedInDate: 1491606000000,
-      children: [
-        { name: 'Jason', age: 12 },
-        { name: 'Roben', age: 7 },
-      ],
-    },
-    {
-      id: 4,
-      fullname: 'ali4',
-      checkedIn: false,
-      checkedInDate: null,
-      children: null,
-    },
-  ];
+export class PassengerDashboardComponent implements OnInit {
+  private passengers: Passenger[];
+  constructor() {}
+  ngOnInit() {
+    console.log('onInit');
+    this.passengers = [
+      {
+        id: 1,
+        fullname: 'ali1',
+        checkedIn: true,
+        checkedInDate: 1490742000000,
+        children: [
+          { name: 'Jack', age: 10 },
+          { name: 'Nana', age: 5 },
+        ],
+      },
+      {
+        id: 2,
+        fullname: 'ali2',
+        checkedIn: false,
+        checkedInDate: null,
+        children: null,
+      },
+      {
+        id: 3,
+        fullname: 'ali3',
+        checkedIn: true,
+        checkedInDate: 1491606000000,
+        children: [
+          { name: 'Jason', age: 12 },
+          { name: 'Roben', age: 7 },
+        ],
+      },
+      {
+        id: 4,
+        fullname: 'ali4',
+        checkedIn: false,
+        checkedInDate: null,
+        children: null,
+      },
+    ];
+  }
 
   getPassengers() {
     return this.passengers;
