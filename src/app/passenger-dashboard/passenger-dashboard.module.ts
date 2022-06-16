d023532a4728685ee75f7ae535abc45214f1af25
aliafn <alimortadafneish36@gmail.com>
2022-06-16 15:50:20 +0300

    1 file modified

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -2,10 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
 
 @NgModule({
-  declarations: [PassengerDashboardComponent],
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerDetailComponent,
+    PassengerCountComponent,
+  ],
   imports: [CommonModule, FormsModule],
   exports: [PassengerDashboardComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
