2ac77b860a97db30083bdf2d191e7678527db12c
aliafn <alimortadafneish36@gmail.com>
2022-06-16 16:28:46 +0300

    4 files modified

--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-detail',
   templateUrl: './passenger-detail.component.html',
+  styleUrls: [
+    '../../containers/passenger-dashboard/passenger-dashboard.component.scss',
+  ],
 })
 export class PassengerDetailComponent {
+  @Input()
+  detail: Passenger;
   constractor() {}
 }
