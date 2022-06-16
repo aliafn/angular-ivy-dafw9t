8457e021d588e847f6b38107bd6b94bc230afbd7
aliafn <alimortadafneish36@gmail.com>
2022-06-16 14:39:03 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ interface Passenger {
   checkedIn: boolean;
   checkedInDate: number | null;
   checkedInNotExist?: boolean;
+  children: Child[] | null;
+}
+interface Child {
+  name: string;
+  age: number;
 }
 
 @Component({
@@ -20,24 +25,34 @@ export class AppComponent {
       fullname: 'ali1',
       checkedIn: true,
       checkedInDate: 1490742000000,
+      children: [
+        { name: 'Jack', age: 10 },
+        { name: 'Nana', age: 5 },
+      ],
     },
     {
       id: 2,
       fullname: 'ali2',
       checkedIn: false,
       checkedInDate: null,
+      children: null,
     },
     {
       id: 3,
       fullname: 'ali3',
       checkedIn: true,
       checkedInDate: 1491606000000,
+      children: [
+        { name: 'Jason', age: 12 },
+        { name: 'Roben', age: 7 },
+      ],
     },
     {
       id: 4,
       fullname: 'ali4',
       checkedIn: false,
       checkedInDate: null,
+      children: null,
     },
   ];
 
