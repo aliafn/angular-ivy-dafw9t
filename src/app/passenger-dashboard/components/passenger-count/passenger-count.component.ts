65ba4de03928f4ee87f30f8128d4ef421c1f39e8
aliafn <alimortadafneish36@gmail.com>
2022-06-16 16:06:21 +0300

    3 files modified

--- src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
 
 @Component({
   selector: 'passenger-count',
   templateUrl: './passenger-count.component.html',
 })
 export class PassengerCountComponent {
+  @Input()
+  items: Passenger[];
   constractor() {}
+  checkedInCount(): number {
+    if (!this.items) {
+      return;
+    } else {
+      return this.items.filter((passenger: Passenger) => {
+        return passenger.checkedIn;
+      }).length;
+    }
+  }
 }
